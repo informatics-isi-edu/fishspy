7518f18269587c787dc9ef948efebaf93124c1b2
mikedarcy <mikedarcy@users.noreply.github.com>
2017-03-21 19:09:18 -0700

    Prototype upload tool for synapse based on deriva-py APIs.

---
+++ uploader/setup.py
@@ -0,0 +1,46 @@
+#
+# Copyright 2017 University of Southern California
+# Distributed under the GNU GPL 3.0 license. See LICENSE for more info.
+#
+
+""" Installation script for Fishspy Upload
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="fishspy_upload",
+    description="Fishspy Upload Tools",
+    url='https://github.com/informatics-isi-edu/fishspy/uploader',
+    maintainer='USC Information Sciences Institute ISR Division',
+    maintainer_email='misd-support@isi.edu',
+    version="0.1.0",
+    packages=find_packages(),
+    package_data={'uploader': ['conf/*.*']},
+    entry_points={
+        'console_scripts': [
+            'fishspy-upload = uploader.upload:main'
+        ]
+    },
+    requires=[
+        'os',
+        'sys',
+        'logging',
+        'deriva_common',
+        'deriva_io'],
+    license='Apache 2.0',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        "Operating System :: POSIX",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ]
+)
+
---
+++ uploader/uploader/upload.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python
+import os
+import sys
+import logging
+from deriva_common import read_config, read_credentials, init_logging, format_exception, urlquote
+from deriva_io.deriva_upload import DerivaUpload
+
+
+class FishspyUpload(DerivaUpload):
+
+    behavior_map = dict()
+    invalid_accessions = list()
+
+    def __init__(self, config, credentials):
+        DerivaUpload.__init__(self, config, credentials)
+
+    def cleanup(self):
+        self.behavior_map.clear()
+        self.invalid_accessions = []
+
+    def _getSubjectID(self, accession):
+        """
+        Helper function that queries the catalog to get a subject id for a given accession.
+        :param accession:
+        :return: the subject_id, or None
+        """
+        subject_id = self.behavior_map.get(accession)
+        if not subject_id and accession not in self.invalid_accessions:
+            try:
+                logging.debug("Validating accession: %s" % accession)
+                path = '/attribute/A:=Zebrafish:Behavior/ID=%s/$A/Subject@sort(Subject)?limit=1' % \
+                       urlquote(accession, '')
+                r = self.catalog.get(path)
+                resp = r.json()
+                if len(resp) > 0:
+                    subject_id = resp[0]['Subject']
+                    self.behavior_map[accession] = subject_id
+            except:
+                (etype, value, traceback) = sys.exc_info()
+                logging.error(format_exception(value))
+
+        return subject_id
+
+    def uploadFile(self, file_path, asset_mapping, callback=None):
+        """
+
+        :param file_path:
+        :param asset_mapping:
+        :param callback:
+        :return:
+        """
+        logging.info("Uploading file: [%s]" % file_path)
+
+        # 1. Retrieve the subject id for the matched accession from the catalog
+        accession = os.path.splitext(os.path.basename(file_path))[0]
+        subject_id = self._getSubjectID(accession)
+        if not subject_id:
+            self.invalid_accessions.append(accession)
+            logging.warn("Ignoring file [%s] due to invalid target accession: %s" % (file_path, accession))
+            return False
+
+        # 2. Assemble the attributes used for the upload
+        file_name = self.getFileDisplayName(file_path, asset_mapping)
+        content_type = self.guessContentType(file_path)
+        hashes = self.getFileHashes(file_path, asset_mapping.get('checksum_types', ['md5']))
+        hash_type = list(hashes.keys())[0]
+        hash_base64 = hashes[hash_type][1]
+
+        # 3. Perform the hatrac upload -- duplicates (based on object name and md5 hash) will not be uploaded
+        url = None
+        try:
+            path = "/".join([asset_mapping['hatrac_namespace'], subject_id, file_name])
+            url = self.store.put_loc(path,
+                                     file_path,
+                                     {"Content-Type": content_type},
+                                     hash_base64,
+                                     chunked=True,
+                                     create_parents=True,
+                                     allow_versioning=False,
+                                     callback=callback)
+        except:
+            (etype, value, traceback) = sys.exc_info()
+            logging.error("Unable to upload file: [%s] - %s" % (file_path, format_exception(value)))
+
+        if not url:
+            return False
+
+        # 4. update the record in the catalog -- fail if there is already an existing entry for this file
+        catalog_table = asset_mapping['catalog_table']
+        return self._catalogRecordUpdate(catalog_table,
+                                         {"ID": accession, "Raw URL": None},
+                                         {"ID": accession, "Raw URL": url})
+
+
+def upload(path):
+    init_logging(level=logging.DEBUG)
+    config = read_config(os.path.join('conf', 'config.json'))
+    credentials = read_credentials(os.path.join('conf', 'credentials.json'))
+    fs_upload = FishspyUpload(config, credentials)
+    if fs_upload.scanDirectory(path, False):
+        fs_upload.uploadFiles()
+    if fs_upload.failed_uploads:
+        logging.warning("The following file(s) failed to upload due to errors:\n\n%s\n" %
+                        '\n'.join(sorted(fs_upload.failed_uploads)))
+    if fs_upload.skipped_uploads:
+        logging.warning("The following file(s) were skipped because they did not satisfy the matching criteria "
+                        "of the configuration:\n\n%s" % '\n'.join(sorted(fs_upload.skipped_uploads)))
+    fs_upload.cleanup()
+
+if __name__ == '__main__':
+    sys.exit(upload(sys.argv[1]))
