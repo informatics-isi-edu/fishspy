b5bf11b9fe2b10cdfcda6a267ab0da8d1056861b
Karl Czajkowski <karlcz@isi.edu>
2019-10-18 10:28:09 -0700

    bugfix: fishspy-plot-cohort summary csv should be written to dump dir

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from distutils.core import setup
 setup(
     name="fishspy",
     description="zebrafish video analysis",
-    version="20191017.0",
+    version="20191017.1",
     scripts=[
         "bin/fishspy-analyze-movie",
         "bin/fishspy-plot-results",
